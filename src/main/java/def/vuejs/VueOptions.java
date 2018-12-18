7405e1e72f8c2585a4fa60da33c1b12c0da6d4dc
Pascal HERAUD <pascal.heraud@laroueverte.com>
2018-12-18 15:40:00 +0100

    Added updated hook

--- src/main/java/def/vuejs/VueOptions.java
+++ src/main/java/def/vuejs/VueOptions.java
@@ -1,11 +1,17 @@
 package def.vuejs;
 
 import jsweet.lang.Interface;
+import jsweet.lang.Optional;
 
 @Interface
 public class VueOptions {
 
 	public String el;
 	public Object data;
+
+	@Optional
 	public Object methods;
+
+	@Optional
+	public Runnable updated;
 }
