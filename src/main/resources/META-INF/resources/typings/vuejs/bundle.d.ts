dd5692ce9a508c9c44a1af9b0c0cf972723674c4
Pascal HERAUD <pascal.heraud@laroueverte.com>
2018-12-19 16:46:29 +0100

    Removed versioning of typescript file to avoid multiple versions

---
+++ src/main/resources/META-INF/resources/typings/vuejs/bundle.d.ts
@@ -0,0 +1,16 @@
+/* Generated from Java with JSweet 2.0.0-SNAPSHOT - http://www.jsweet.org */
+declare class Vue<T> {
+    public constructor(options : VueOptions);
+}
+
+interface VueOptions {
+    el : string;
+
+    data : any;
+
+    methods? : any;
+
+    updated? : () => void;
+}
+
+
